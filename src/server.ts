1a9acc7e89a47b9883792ac4435e0696b55e6944
Oleksandr Bystrikov <coder@yakviter.com.ua>
2026-04-09 14:10:32 +0200

    fix: remove fragile import shims

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
-// Normal Imports
 import { createApp } from "astro/app/entrypoint";
 import { setGetEnv } from "astro/env/setup";
 setGetEnv((key) => Deno.env.get(key));
 import * as options from "virtual:@deno/astro-adapter:config";
-
-// @ts-expect-error
-import { fromFileUrl, serveFile } from "@deno/astro-adapter/__deno_imports.ts";
+import { JSR_STD_HTTP_FILE_SERVER, JSR_STD_PATH } from "./index"
 
 const app = createApp();
 
@@ -27,12 +24,15 @@ function removeTrailingForwardSlash(path: string) {
   return path.endsWith("/") ? path.slice(0, path.length - 1) : path;
 }
 
-function start() {
+async function start() {
+  const { serveFile } = await import(JSR_STD_HTTP_FILE_SERVER);
+  const { fromFileUrl } = await import(JSR_STD_PATH);
+
   // undefined = not yet loaded, null = not installed
   let trace: import("@opentelemetry/api").TraceAPI | null | undefined;
 
   const clientRoot = new URL(options.relativeClientPath, import.meta.url);
-  const handler = async (request: Request, handlerInfo: any) => {
+  const handler: Deno.ServeHandler = async (request, handlerInfo) => {
     if (trace === undefined) {
       try {
         trace = (await import("@opentelemetry/api")).trace;
@@ -67,8 +67,8 @@ function start() {
     let fileResp = await serveFile(request, fromFileUrl(localPath));
 
     // Attempt to serve `index.html` if 404
-    if (fileResp.status == 404) {
-      let fallback;
+    if (fileResp.status === 404) {
+      let fallback: URL | undefined;
       for await (const file of getPrerenderedFiles(clientRoot)) {
         const pathname = file.pathname.replace(/\/(index)?\.html$/, "");
         if (removeTrailingForwardSlash(localPath.pathname).endsWith(pathname)) {
@@ -82,7 +82,7 @@ function start() {
     }
 
     // If the static file can't be found
-    if (fileResp.status == 404) {
+    if (fileResp.status === 404) {
       // Render the astro custom 404 page
       const response = await app.render(request);
 
